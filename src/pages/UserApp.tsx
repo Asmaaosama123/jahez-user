d6f62db198f246a6b422385116a539089af5b1cb
Asmaa Osama <ao9191360@gmail.com>
2025-12-19 20:51:52 +0200

    update

--- src/pages/UserApp.tsx
+++ src/pages/UserApp.tsx
@@ -3,6 +3,7 @@ import Home from "./Home";
 import RestaurantPage from "./RestaurantPage";
 import Cart from "./Cart";
 import PublicOrder from "./PublicOrder";
+import PrivacyPolicy from "./privacy";
 
 export default function UserApp() {
   return (
@@ -11,6 +12,7 @@ export default function UserApp() {
       <Route path="/restaurant/:name" element={<RestaurantPage />} />
       <Route path="/cart" element={<Cart />} />
       <Route path="/public-order/:orderId" element={<PublicOrder />} />
+      <Route path="/privacy-policy" element={<PrivacyPolicy />} />
     </Routes>
   );
 }
---
+++ src/pages/privacy.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const privacy: React.FC = () => {
+  return (
+    <div style={{ padding: "20px", lineHeight: "1.8" }}>
+      <h1>Politique de confidentialité – Application Jahez</h1>
+
+      <p>
+        Jahez respecte la vie privée de ses utilisateurs et s&apos;engage à
+        protéger leurs données.
+      </p>
+
+      <p>
+        Seules les informations essentielles au traitement et à la livraison des
+        commandes, telles que le nom, le numéro de téléphone et l&apos;adresse de
+        livraison, sont collectées.
+      </p>
+
+      <p>
+        Les données des utilisateurs ne sont partagées avec aucun tiers, sauf si
+        la loi l&apos;exige ou à des fins de livraison.
+      </p>
+
+      <p>
+        Les données sont sécurisées par des mesures de protection appropriées.
+      </p>
+    </div>
+  );
+};
+
+export default privacy;
